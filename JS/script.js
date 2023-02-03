3a48233b531e88356bdf6412bf850d14d2cb66f9
André Portugal <depaula_andrep@hotmail.com>
2023-02-02 21:20:48 -0300

    Modificação inicial de layout e inclusão de código

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+//Função que busca o endereço a partir do CEP
 function consultaEndereco() {
   let cep = document.querySelector('#cep').value;
   let url = `https://viacep.com.br/ws/${cep}/json/`;
@@ -9,6 +10,34 @@ function consultaEndereco() {
 
 function mostrarEndereco(data) {
   if (data.erro) {
+    resultadoEndereco.innerHTML = 'CEP inexistente';
+  } else {
+    let resultadoEndereco = document.querySelector(`#resultado`);
+    resultadoEndereco.innerHTML = `${data.logradouro}
+  ${data.complemento}
+  ${data.bairro}
+  ${data.localidade} - ${data.uf}`;
+  }
+}
+
+//Função que busca o CEP a partir do endereço
+function consultaCep() {
+  alert('ERRO! Código em construção');
+
+  /*let uf = document.querySelector('#uf').value;
+  let rua = document.querySelector('#rua').value;
+  let cidade = document.querySelector('#cidade').value;
+  let url = `https://viacep.com.br/ws/${uf + '/' + cidade + '/' + rua}/json/`;
+  console.log(url);
+
+  fetch(url).then(function (response) {
+    response.json().then(mostrarCep);
+  });
+  */
+}
+/*
+function mostrarEndereco(data) {
+  if (data.erro) {
     resultado.innerHTML = 'CEP inexistente';
   } else {
     let resultado = document.querySelector(`#resultado`);
@@ -17,4 +46,4 @@ function mostrarEndereco(data) {
   Bairro: ${data.bairro}
   Localidade: ${data.localidade} - UF: ${data.uf}`;
   }
-}
+}*/
